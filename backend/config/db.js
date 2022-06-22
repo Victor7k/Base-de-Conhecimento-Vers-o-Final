dc4e8b29218a7775cf155af8a03bbf30f52c1f37
Victor <victor22158403@hotmail.com>
2022-06-22 17:51:40 -0300

    VersaoCompleta

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,20 @@
 const config = require('../knexfile.js')
-const knex = require('knex')(config)
 
-knex.migrate.latest([config])
+const knex = require('knex')({
+    client: 'postgres',
+    connection: async () => {
+        return {
+            host: '127.0.0.1',
+            port: 5432,
+            user: 'postgres',
+            password: '123456',
+            database: 'knowledge_final',
+            expirationChecker: () => {
+                return Date.now() * 5;
+            }
+        };
+    }
+});
+
+// knex.migrate.latest([config])
 module.exports = knex
